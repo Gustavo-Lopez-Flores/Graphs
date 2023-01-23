4e21c27a15aa960338ab4c772568f29c16f66e60
Gustavo Lopez Flores <104476623+Gustavo-Lopez-Flores@users.noreply.github.com>
2023-01-23 18:23:08 -0400

    Add files via upload

---
+++ Lista Adjacencia/ContaNoAdjacenteLista.py
@@ -0,0 +1,30 @@
+class Grafo:
+  def __init__(self, vertices):
+    print('Lista:')
+    self.vertices = vertices
+    self.grafo = [[]for i in range(self.vertices)]
+  def adiciona_aresta(self, u, v):
+    self.grafo[u-1].append(v-1)
+    self.grafo[v-1].append(u-1)
+  def imprime_lista(self):
+    for i in range(self.vertices):
+      print(f'{i+1}:', end=' ')
+      for j in self.grafo[i]:
+        print(f'{j+1} -', end=' ')
+      print('')
+  def conta_nos(self, no):
+    self.no = no-1
+    print(f'Nos adjascentes ao no ({no}):')
+    print(f'{no}:', end=' ')
+    for j in self.grafo[self.no]:
+       print(f'{j+1} -', end=' ')
+    print('')
+
+g = Grafo(4)
+g.adiciona_aresta(1,2)
+g.adiciona_aresta(2,3)
+g.adiciona_aresta(2,4)
+g.adiciona_aresta(3,1)
+g.adiciona_aresta(4,1)
+g.imprime_lista()
+g.conta_nos(1)
